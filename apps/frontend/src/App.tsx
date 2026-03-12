26331c9e252fe86de59866c586cf3e373a8cecf6
Alexandr <AlexandrKsenz@gmail.com>
2026-03-12 21:42:58 +0100

    test v3

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -26,7 +26,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>{backendMessage}</h2>
-      <h2><strong>Tests on github workflow v3</strong></h2>
+      <h2><strong>Tests on github workflow v4</strong></h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
