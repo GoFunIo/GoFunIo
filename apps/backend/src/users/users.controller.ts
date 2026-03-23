3ffadcb7e8f5b4e98ce95be96f59e8e6ab662eef
Sylwester Sobczak <sylwester.sob95@gmail.com>
2026-03-23 12:06:40 +0100

    Add user retrieval endpoints and service methods for fetching users; include HTTP request examples

--- apps/backend/src/users/users.controller.ts
+++ apps/backend/src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { User } from './users.entity';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get('users/:id')
+  getUser(@Param('id') id: number): Promise<User | null> {
+    return this.usersService.findOne(id);
+  }
+
+  @Get('users')
+  getUsers(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+}
--- apps/backend/src/users/users.service.ts
+++ apps/backend/src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  async findOne(id: number): Promise<User | null> {
+    return this.usersRepository.findOneBy({ id });
+  }
+
+  async create(user: User): Promise<User> {
+    return this.usersRepository.save(user);
+  }
+}
