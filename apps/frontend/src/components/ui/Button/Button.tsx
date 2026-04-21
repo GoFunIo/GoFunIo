ca2db2aee34baaeba5f9256de62934729a4dd5e6
Alexandr <AlexandrKsenz@gmail.com>
2026-04-21 18:02:37 +0200

    button UI component styles

--- apps/frontend/src/components/ui/Button/Button.tsx
+++ apps/frontend/src/components/ui/Button/Button.tsx
@@ -1,13 +1,51 @@
+import classNames from 'classnames';
+
 type Props = {
   children: React.ReactNode;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
-  type?: 'button' | 'submit';
+  type?: 'button' | 'submit' | 'reset';
   disabled?: boolean;
+  variant?: 'default' | 'outline';
+  style?: React.CSSProperties;
 };
 
-export const Button = ({ children, onClick, type = 'button', disabled = false }: Props) => {
+export const Button = ({
+  children,
+  onClick,
+  type = 'button',
+  disabled = false,
+  variant = 'default',
+  style,
+}: Props) => {
+  const mainButtonStyles = `
+    min-w-[190px] w-fit min-h-[50px] rounded-[7px] px-[8px]
+    text-[16px] leading-[14px] font-semibold text-center
+    cursor-pointer custom-transition
+    hover:shadow-[0_3px_13px_0_rgba(0,0,0,0.2)]
+  `;
+  const defaultButtonStyles = `
+    bg-primary hover:bg-secondary text-white
+  `;
+  const outlineButtonStyles = `
+    bg-white text-primary hover:text-secondary
+    border border-primary hover:border-secondary
+  `;
+  const disabledButtonStyles = `
+    pointer-events-none opacity-50 cursor-default
+  `;
+
   return (
-    <button onClick={onClick} type={type} disabled={disabled}>
+    <button
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+      className={classNames(mainButtonStyles, {
+        [defaultButtonStyles]: variant === 'default',
+        [outlineButtonStyles]: variant === 'outline',
+        [disabledButtonStyles]: disabled,
+      })}
+      style={style}
+    >
       {children}
     </button>
   );
--- apps/frontend/src/routes/index.tsx
+++ apps/frontend/src/routes/index.tsx
@@ -1,9 +1,26 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { Button } from 'src/components/ui/Button';
 
 export const Route = createFileRoute('/')({
   component: Index,
 });
 
 function Index() {
-  return <div>Hello "/Index"!</div>;
+  return (
+    <div>
+      <h3 className="pb-8">Hello "/Index"!</h3>
+      <div className="flex flex-col gap-4">
+        <Button onClick={() => {}}>Primary</Button>
+        <Button disabled onClick={() => {}}>
+          Primary
+        </Button>
+        <Button variant="outline" onClick={() => {}}>
+          Primary
+        </Button>
+        <Button disabled variant="outline" onClick={() => {}}>
+          Primary
+        </Button>
+      </div>
+    </div>
+  );
 }
