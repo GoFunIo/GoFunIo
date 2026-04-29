f2e376b7786b448efc40977667bd8d18b43adee9
Alexandr <AlexandrKsenz@gmail.com>
2026-04-29 20:58:10 +0200

    added verify email page

--- apps/frontend/src/components/features/auth/AuthWrapper.tsx
+++ apps/frontend/src/components/features/auth/AuthWrapper.tsx
@@ -1,3 +1,4 @@
+import { Link } from '@tanstack/react-router';
 import React from 'react';
 import { getImage } from 'src/utils/getImage';
 
@@ -5,13 +6,25 @@ type Props = {
   title: string;
   subtitle: string;
   children: React.ReactNode;
+  isSuccess?: boolean;
 };
 
-export const AuthWrapper = ({ title, subtitle, children }: Props) => {
+export const AuthWrapper = ({ title, subtitle, children, isSuccess = false }: Props) => {
   return (
     <section className="px-[20px] bg-bg-section flex items-center justify-center h-full">
       <div className="bg-white max-w-[560px] w-full rounded-[15px] shadow-[0_4px_13px_0_rgba(0,0,0,0.2)] md:py-[70px] md:px-[50px] py-[50px] px-[30px]">
-        <img src={getImage('logo.svg')} alt="Logo company" className="m-auto pb-[24px]" />
+        <Link to="/">
+          <img src={getImage('logo.svg')} alt="Logo company" className="m-auto mb-[24px]" />
+        </Link>
+        {isSuccess && (
+          <div className="w-[61px] h-[61px] rounded-full bg-success/20 relative m-auto mb-[24px]">
+            <img
+              src={getImage('success-icon.svg')}
+              alt="Success verify email"
+              className="absolute top-1/2 left-[51%] -translate-x-1/2 -translate-y-1/2"
+            />
+          </div>
+        )}
         <h3 className="text-center pb-[16px]">{title}</h3>
         <p className="text-center">{subtitle}</p>
 
