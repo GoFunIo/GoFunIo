33b1e6629c050c7e8f09ba20c2d0ae33d2dbea8e
Wioleta Lip <wiolip@hotmail.com>
2026-07-23 23:35:02 +0200

    - poprawiono loadery w dashbord - zoptymalizowano  liczenie statystyk w buckets - podpieto uzytkownika do dashbord cards - uzależniono ładowanie danych od roli ADMIN

--- apps/frontend/src/hooks/useTeam.ts
+++ apps/frontend/src/hooks/useTeam.ts
@@ -1,9 +1,13 @@
 import { getTeam } from '@/features/dashboard/api/team.api';
 import { useQuery } from '@tanstack/react-query';
+import { useUser } from './useUser';
 
 export const useTeam = () => {
+  const { data: user } = useUser();
+
   return useQuery({
     queryKey: ['team'],
     queryFn: getTeam,
+    enabled: user?.role === 'ADMIN',
   });
 };
